3a16f4deeb091a7e2d95d7a090f9164c66b32b1f
Timo Kössler <info@timokoessler.de>
2026-06-10 09:52:14 +0200

    feat: Basic PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { compression } from "vite-plugin-compression2";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,41 @@ export default defineConfig(({ mode }) => ({
 			autoCodeSplitting: true,
 		}),
 		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: "Marp Collab",
+				short_name: "Marp Collab",
+				description: "A collaborative presentation editor powered by Marp",
+				theme_color: "#2266a7",
+				icons: [
+					{
+						src: "pwa-64x64.png",
+						sizes: "64x64",
+						type: "image/png",
+					},
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
+		}),
 		tailwindcss(),
 		compression({
 			algorithms: ["gzip", "brotli", "zstd"],
